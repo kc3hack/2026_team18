3c6a360768e60fbb21ba3a1d5261ebec32920b57
tyta- <K7a28ke@outlook.jp>
2026-02-17 00:15:00 +0900

    style: インデント幅を変更

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,43 +1,40 @@
 import 'package:mikata/models/post.dart';
 
 class User {
-// public member
-	String userName;
+  // public member
+    String userName;
 
-// private member
-	final List<Post> _userPosts = [];
-	final List<Post> _bookmarkPost = [];
-	final List<Post> _likePost = [];
+  // private member
+    final List<Post> _userPosts = [];
+    final List<Post> _bookmarkPost = [];
+    final List<Post> _likePost = [];
 
-//public method
-	User({
-		required this.userName,
-	});
+    //public method
+    User({required this.userName});
 
-	void addUserPost(Post post) {
-		_userPosts.add(post);
-	}
+    void addUserPost(Post post) {
+        _userPosts.add(post);
+    }
 
-	void removeUserPost(Post post) {
-		_userPosts.remove(post);
-	}
+    void removeUserPost(Post post) {
+        _userPosts.remove(post);
+    }
 
-	void addBookmark(Post post) {
-		_bookmarkPost.add(post);
-	}
+    void addBookmark(Post post) {
+        _bookmarkPost.add(post);
+    }
 
-	void remove(Post post) {
-		_bookmarkPost.remove(post);
-	}
+    void remove(Post post) {
+        _bookmarkPost.remove(post);
+    }
 
-	void addLikePost(Post post) {
-		_likePost.add(post);
-	}
+    void addLikePost(Post post) {
+        _likePost.add(post);
+    }
 
-	void removeLikePost(Post post) {
-		_likePost.remove(post);
-	}
-// private method
+    void removeLikePost(Post post) {
+        _likePost.remove(post);
+    }
 
-
+  // private method
 }
