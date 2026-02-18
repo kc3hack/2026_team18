f53a37293dc31a6ee2d0630b695c0cac753b286f
tyta- <K7a28ke@outlook.jp>
2026-02-19 00:30:04 +0900

    feat: タイムラインクラスに投稿の返信機能と著者名更新機能を追加。特定のポストのリストを取得する方法を追加

--- lib/models/timeline.dart
+++ lib/models/timeline.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 // Project imports:
 import 'package:mikata/models/file_utils.dart';
 import 'package:mikata/models/post.dart';
+import 'package:mikata/models/account.dart';
 
 class Timeline {
 // public member
@@ -16,13 +17,27 @@ class Timeline {
     List<Post> get timeline => _timeline;
 
     List<Post> getLikePost() {
-        final List<Post> result = _timeline.where((i) => i.isLike).toList();
-        return result;
+        return _timeline.where((i) => i.isLike).toList();
     }
 
     List<Post> getBookmarkPost() {
-        final List<Post> result = _timeline.where((i) => i.isBookmark).toList();
-        return result;
+        return _timeline.where((i) => i.isBookmark).toList();
+    }
+
+    List<Post> getPostByAccountUUID(String uuid) {
+        return _timeline.where((i) => i.authorUuid == uuid).toList();
+    }
+
+    List<Post> getPostByAccount(Account account) {
+        return getPostByAccountUUID(account.accountUUID);
+    }
+
+    List<Post> getReplyPostByParentPostUUID(String uuid) {
+        return _timeline.where((i) => i.parentPostUuid == uuid).toList();
+    }
+
+    List<Post> getReplyPostByParentPost(Post post) {
+        return getReplyPostByParentPostUUID(post.parentPostUuid);
     }
 
     void addPost(Post post) {
@@ -33,6 +48,16 @@ class Timeline {
         _timeline.remove(post);
     }
 
+    void replyPost(Post reply, String parentUUID) {
+        reply.parentPostUuid = parentUUID;
+        addPost(reply);
+    }
+
+    void updateAuthorName(String uuid, String newName) {
+        for (Post i in getPostByAccountUUID(uuid)) {
+            i.authorName = newName;
+        }
+    }
 // private member
     void loadPost() async {
         String? json = await FileIO().loadTextFile("post.json");
