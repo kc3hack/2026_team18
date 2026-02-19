8fb90ec383ebf9157f68d854bc8b8813c115fee1
tyta- <K7a28ke@outlook.jp>
2026-02-19 20:49:37 +0900

    change: loacalPathをpublicに変更

--- lib/models/file_utils.dart
+++ lib/models/file_utils.dart
@@ -9,13 +9,13 @@ import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 
 class FileIO {
-	Future<String> get _loacalPath async {
+	Future<String> get loacalPath async {
 		final directory = await getApplicationDocumentsDirectory();
 		return directory.path;
 	}
 
 	Future<File>  _getFile(String fileName) async {
-		final path = await _loacalPath;
+		final path = await loacalPath;
 		return File('$path/$fileName');
 	}
 
