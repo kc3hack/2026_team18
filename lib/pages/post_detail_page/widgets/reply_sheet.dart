2f9d0af77967d1dcf2ef617342d6294f692e2905
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2026-02-19 17:41:20 +0900

    refactor: ReplySheetウィジェットを追加し、返信機能を実装

---
+++ lib/pages/post_detail_page/widgets/reply_sheet.dart
@@ -0,0 +1,52 @@
+part of '../post_detail_page.dart';
+
+class ReplySheet extends HookConsumerWidget {
+  const ReplySheet({super.key, required this.parentPostId});
+
+  final String parentPostId;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final user = ref.watch(userAccountProvider);
+
+    final inputController = useTextEditingController();
+
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        border: Border(top: BorderSide(color: Colors.grey.shade300)),
+        color: colorScheme.surface,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: inputController,
+              decoration: InputDecoration(hintText: "返信を入力"),
+              maxLines: 1,
+            ),
+          ),
+
+          OutlinedButton(
+            onPressed: (user.value == null)
+                ? null
+                : () {
+                    ref.read(timelineProvider.notifier)
+                      ..addPost(
+                        Post(
+                          authorName: user.value!.accountName,
+                          authorUuid: user.value!.accountID,
+                          content: inputController.text,
+                          postDate: DateTime.now(),
+                        ),
+                      )
+                      ..fetchTimeline();
+                  },
+            child: Text("返信"),
+          ),
+        ],
+      ),
+    );
+  }
+}
