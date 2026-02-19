57c16c0cd221e85cf1c320d25e96c9c5e6e40287
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2026-02-19 18:32:00 +0900

    refactor: 不要なインポートとコメントを削除し、親投稿のない投稿のみを表示するように変更

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -6,7 +6,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // Project imports:
 import 'package:mikata/pages/home_page/widgets/post_box.dart';
-import 'package:mikata/providers/posts_provider.dart';
 import 'package:mikata/providers/timeline_provider.dart';
 import 'package:mikata/widgets/custom_appbar.dart';
 
@@ -16,7 +15,6 @@ class HomePage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final timelineAsync = ref.watch(timelineProvider);
-    // final timelineAsync = ref.watch(postsProvider);
 
     return Scaffold(
       appBar: CustomAppbar(title: const Text("Home")),
@@ -25,21 +23,22 @@ class HomePage extends HookConsumerWidget {
           await ref.read(timelineProvider.notifier).fetchTimeline();
         },
         child: timelineAsync.when(
-          data: (timeline) => ListView.separated(
-            itemCount: timeline.timeline.length,
-            itemBuilder: (context, index) {
-              final post = timeline.timeline[index];
-              return PostBox(post: post);
-            },
-            // itemCount: timeline.length,
-            // itemBuilder: (context, index) {
-            //   final post = timeline[index];
-            //   return PostBox(post: post);
-            // },
-            separatorBuilder: (context, index) {
-              return const Divider(height: 1);
-            },
-          ),
+          data: (timeline) {
+            final noParentPosts = timeline.timeline
+                .where((post) => post.parentPostUUID.isEmpty)
+                .toList();
+
+            return ListView.separated(
+              itemCount: noParentPosts.length,
+              itemBuilder: (context, index) {
+                final post = noParentPosts[index];
+                return PostBox(post: post);
+              },
+              separatorBuilder: (context, index) {
+                return const Divider(height: 1);
+              },
+            );
+          },
           loading: () => const Center(child: CircularProgressIndicator()),
           error: (error, stack) => Center(child: Text("Error: $error")),
         ),
--- lib/pages/post_detail_page/post_detail_page.dart
+++ lib/pages/post_detail_page/post_detail_page.dart
@@ -28,8 +28,6 @@ class PostDetailPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final replyListCount = post.replyCount;
 
-    final colorScheme = Theme.of(context).colorScheme;
-
     return Scaffold(
       appBar: CustomAppbar(title: const Text("投稿の詳細")),
       body: ListView.separated(
